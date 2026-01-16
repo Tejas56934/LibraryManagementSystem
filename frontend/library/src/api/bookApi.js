ab7956c54b64b570ad0a8c18659c699d354a2abe
Tejas Pandit <tejaspandit623@gmail.com>
2026-01-16 23:18:02 +0530

    Change API URL to production endpoint

--- frontend/library/src/api/bookApi.js
+++ frontend/library/src/api/bookApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/v1/';
+const API_URL = 'https://lms-backend-tejas.onrender.com/api/v1/';
 
 // Utility function to get the current user's token for protected endpoints
 const getAuthHeaders = () => {
