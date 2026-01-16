2f1b13e8088bbe32629041d496be33b0beb6b45a
Tejas Pandit <tejaspandit623@gmail.com>
2026-01-16 23:14:40 +0530

    "https://lms-backend-tejas.onrender.com"

--- frontend/library/src/api/authApi.js
+++ frontend/library/src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/v1/auth/';
+const API_URL = 'https://lms-backend-tejas.onrender.com/api/v1/auth/';
 
 const login = async (username, password) => {
   const response = await axios.post(API_URL + 'login', {
